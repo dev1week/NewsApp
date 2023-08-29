d1c6a45995d811c4bd31568f3bc02233b946e5e9
dev1week <rondo2860@gmail.com>
2023-08-29 23:39:49 +0900

    #8 -2 recoil에 async storage 적용

--- states/atomLinkList.js
+++ states/atomLinkList.js
@@ -1,8 +1,25 @@
-import { atom  } from "recoil"
+import { atom } from "recoil";
+import { getItem, removeItem, setItem } from "../utils/AsyncStorageUtils";
 
-export const atomLinkList =atom({   
-    key:'MAIN/LINK_LIST',
-    default:{
-        list:[],
+const asyncStorageEffect =
+  (key) =>
+  async ({ setSelf, onSet }) => {
+    const savedValue = await getItem(key);
+
+    if (savedValue !== null) {
+      setSelf(JSON.parse(savedValue));
     }
-})
+
+    onSet((newValue, _, isReset) => {
+      console.log("온셋", newValue);
+      isReset ? removeItem(key) : setItem(key, JSON.stringify(newValue));
+    });
+  };
+
+export const atomLinkList = atom({
+  key: "MAIN/LINK_LIST",
+  default: {
+    list: [],
+  },
+  effects: [asyncStorageEffect("MAIN/LINK_LIST")],
+});
