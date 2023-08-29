cec3ce415ea017a61bfbce09675172b681cd137a
hanju <the_1_week@naver.com>
2023-08-29 14:09:47 +0900

    #3 -5 목록 페이지 추가 버튼 구현

--- screens/LinkListScreens.js
+++ screens/LinkListScreens.js
@@ -3,8 +3,11 @@ import {Header} from "../components/Header/Header"
 import { View, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Button } from "../components/Button";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Icon } from "../components/Icon";
 export const LinkListScreen=()=>{
     const navigation = useNavigation();
+    const safeAreaInset = useSafeAreaInsets(); 
 
     const onPressButton = useCallback(()=>{
         navigation.navigate("LinkDetail")
@@ -21,6 +24,7 @@ export const LinkListScreen=()=>{
                     <Header.Title title='LINK LIST'> </Header.Title>
                 </Header.Group>
             </Header>
+        {/* 화면목록 */}
             <View style={{flex:1}}>
                 <Button onPress={onPressButton}>
                     <Text> Link Detail 이동</Text>
@@ -28,7 +32,14 @@ export const LinkListScreen=()=>{
                 <Button onPress={onPressAddButton}>
                     <Text> Add Link 이동 </Text>
                 </Button>
-
+            </View>
+        {/* 추가버튼 */}
+            <View style={{position : 'absolute', right:24, bottom:24+safeAreaInset.bottom}}>
+                <Button onPress={onPressAddButton}>
+                    <View style={{width:52, height:52, borderRadius:26, alignItems:'center', justifyContent:'center', backgroundColor:"black"}}>
+                        <Icon name='add' color='white' size={32}/>
+                    </View>
+                </Button>
             </View>
         </View>
     )
