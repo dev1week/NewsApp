45a3e71c8971a8b71468eb06267292fc9accf078
hanju <the_1_week@naver.com>
2023-08-29 17:24:51 +0900

    #4 -7 recoil에 저장된 url 리스트 목록으로 표현

--- screens/LinkListScreens.js
+++ screens/LinkListScreens.js
@@ -1,13 +1,19 @@
 import React, { useCallback } from "react";
 import {Header} from "../components/Header/Header"
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Button } from "../components/Button";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Icon } from "../components/Icon";
+import { useRecoilValue } from "recoil";
+import { atomLinkList } from "../states/atomLinkList";
+import { Typography } from "../components/Typography";
+import { Spacer } from "../components/Spacer";
 export const LinkListScreen=()=>{
     const navigation = useNavigation();
     const safeAreaInset = useSafeAreaInsets(); 
+
+    const data = useRecoilValue(atomLinkList); 
 
     const onPressButton = useCallback(()=>{
         navigation.navigate("LinkDetail")
@@ -24,16 +30,31 @@ export const LinkListScreen=()=>{
                     <Header.Title title='LINK LIST'> </Header.Title>
                 </Header.Group>
             </Header>
-        {/* 화면목록 */}
-            <View style={{flex:1}}>
-                <Button onPress={onPressButton}>
-                    <Text> Link Detail 이동</Text>
-                </Button>
-                <Button onPress={onPressAddButton}>
-                    <Text> Add Link 이동 </Text>
-                </Button>
-            </View>
-        {/* 추가버튼 */}
+        
+            {/* 화면목록 */}
+            
+            <FlatList 
+                style={{flex:1,}}
+                data={data.list}
+                renderItem={({item})=>{
+                    return(
+                        <View>
+                            <Typography fontSize={20}>
+                                {item.link} 
+                            </Typography>
+                            <Spacer space={4}/>
+                            {/* 타이틀이 없을 경우 공백 처리 */}
+                            <Typography fontSize={14} color='gray'>
+                                {item.title !== ''? `${item.title.slice(0,20)} |` :''}
+                                {new Date(item.createdAt).toLocaleString()}
+                            </Typography>
+                        </View>
+                    )
+                }}
+            />
+
+            {/* 추가버튼 */}
+
             <View style={{position : 'absolute', right:24, bottom:24+safeAreaInset.bottom}}>
                 <Button onPress={onPressAddButton}>
                     <View style={{width:52, height:52, borderRadius:26, alignItems:'center', justifyContent:'center', backgroundColor:"black"}}>
