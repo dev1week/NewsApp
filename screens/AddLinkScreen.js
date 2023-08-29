b6009b9525a83faadd8d8daa61a60074770c1a44
hanju <the_1_week@naver.com>
2023-08-29 13:59:08 +0900

    #3 -3 헤더에 뒤로가기 기능, 각 버튼에 스크린 전환 기능 추가

--- screens/AddLinkScreen.js
+++ screens/AddLinkScreen.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Header } from "../components/Header/Header";
 import { View } from "react-native";
-
+import { useNavigation } from "@react-navigation/native";
+import { Spacer } from "../components/Spacer";
 
 export const AddLinkScreen =()=>{
+    const navigation = useNavigation(); 
+    
+    const onPressBack = useCallback(()=>{
+        navigation.goBack(); 
+    },[])
+
     return(
         <View> 
             <Header>
                 <Header.Group>
+                    
+                    <Header.Icon iconName='arrow-back' onPress={onPressBack}></Header.Icon>
+                    <Spacer space={12} horizontal></Spacer>
                     <Header.Title title = "AddLinkScreen"></Header.Title>
                 </Header.Group>
             </Header>
--- screens/LinkListScreens.js
+++ screens/LinkListScreens.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useCallback } from "react";
 import {Header} from "../components/Header/Header"
 import { View, Text } from "react-native";
-
+import { useNavigation } from "@react-navigation/native";
+import { Button } from "../components/Button";
 export const LinkListScreen=()=>{
+    const navigation = useNavigation();
+
+    const onPressButton = useCallback(()=>{
+        navigation.navigate("LinkDetail")
+    },[])
+
+    const onPressAddButton = useCallback(()=>{
+        navigation.navigate("AddLink")
+    },[])
+
     return(
         <View style={{flex:1}}> 
             <Header>
@@ -10,6 +21,15 @@ export const LinkListScreen=()=>{
                     <Header.Title title='LINK LIST'> </Header.Title>
                 </Header.Group>
             </Header>
+            <View style={{flex:1}}>
+                <Button onPress={onPressButton}>
+                    <Text> Link Detail 이동</Text>
+                </Button>
+                <Button onPress={onPressAddButton}>
+                    <Text> Add Link 이동 </Text>
+                </Button>
+
+            </View>
         </View>
     )
 }
