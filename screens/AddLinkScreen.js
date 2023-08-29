2a1d70f9d826426ffe99d64bdafc14aacec46947
hanju <the_1_week@naver.com>
2023-08-29 17:24:10 +0900

    #4 -5 url 입력 후 recoil에 저장

--- screens/AddLinkScreen.js
+++ screens/AddLinkScreen.js
@@ -9,11 +9,39 @@ import { Typography } from "../components/Typography";
 
 import { SingleLineInput } from "../components/SingleLineInput";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useSetRecoilState } from "recoil";
+import { atomLinkList } from "../states/atomLinkList";
 export const AddLinkScreen =()=>{
     const navigation = useNavigation(); 
     const safeAreaInset = useSafeAreaInsets(); 
 
+    // url 저장 
     const [url, setUrl] = useState('');
+    const updateList = useSetRecoilState(atomLinkList);
+
+    const onPressSave = useCallback(()=>{
+        console.log("test");
+        console.log(url);
+        if(url==='')return;
+
+        updateList((prevState)=>{
+            const list = [
+                {
+                    title:'',
+                    image:'',
+                    link:url,
+                    createdAt: new Date().toISOString(), 
+                }
+            ]
+            return {
+                //기존 list에 추가 
+                list:list.concat(prevState.list)
+            }
+        })
+        setUrl('');
+    }, [url])
+
+    // 뒤로가기 
     const onPressBack = useCallback(()=>{
         navigation.goBack(); 
     },[])
@@ -30,20 +58,21 @@ export const AddLinkScreen =()=>{
             </Header>
             {/* url 입력창 */}
             <View style={{flex:1, alignItems:'center', justifyContent:'center', paddingHorizontal:24}}>
-                <SingleLineInput
-                    value={url}
-                    onChangeText={setUrl}
-                    placeholder='https://example.com'
-                />
+                    <SingleLineInput
+                        value={url}
+                        onChangeText={setUrl}
+                        placeholder='https://example.com'
+                    />
             </View>
 
-            <Button >
+            <Button onPress={onPressSave}>
                 <View style={{backgroundColor: url===''? 'gray' : 'black'}}> 
                     <View style={{height:52, alignItems: 'center', justifyContent: 'center'}}>
                         <Typography color='white' fontSize={18}>저장하기</Typography>
                     </View>
+                    <Spacer space={safeAreaInset.bottom}/>
                 </View>
-                <Spacer space={safeAreaInset.bottom}/>
+               
             </Button>
         </View>
     )
